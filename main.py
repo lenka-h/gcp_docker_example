280dfa6c536c44f473566cf41b4a773633ff768d
lenhy <lenhy@dtu.dk>
2022-01-11 11:13:54 +0100

    spaces

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     # Create a classifier: a support vector classifier
     clf = svm.SVC(gamma=0.001)
 
-    # Split data into 50% train and 50% test subsets
+    # Split data into 50 % train and 50 % test subsets
     X_train, X_test, y_train, y_test = train_test_split(
         data, digits.target, test_size=0.5, shuffle=False
     )
